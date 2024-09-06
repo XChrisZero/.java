311f80646b4327896c25e7a815c535f12eea754f
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-09-06 20:26:16 -0300

    Update Aula.javaasdaDWAS DEsadazxdc
    adfsdzfsfa

--- Aula.java
+++ Aula.java
@@ -1,3 +1,19 @@
+public class Main {
+    public static void main( String[] args){
+
+        new Janela();
+    }   
+}
+
+
+
+
+
+
+
+
+---------------------------------
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
